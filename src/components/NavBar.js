87d34c09fa68c1431c87d6f650f219c705d2fcc3
Federico Delgado <delgadofefe@gmail.com>
2023-02-02 12:30:59 -0300

    Update nav Links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
@@ -5,6 +6,13 @@ import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
     <Navbar expand="md">
       <Container>
@@ -12,13 +20,13 @@ export const NavBar = () => {
           <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#skills">Habilidades</Nav.Link>
-            <Nav.Link href="#projects">Proyectos</Nav.Link>
+          <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Inicio</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Proyectos</Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
