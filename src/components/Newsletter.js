0f6c7fd9cc3d8fed6982772117333f5cab52a2e2
Federico Delgado <delgadofefe@gmail.com>
2023-02-02 22:42:43 -0300

    Add mail submit

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,18 +1,41 @@
-import { Col, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-export const Newsletter = () => {
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
+
+  const clearFields = () => {
+    setEmail('');
+  }
+
   return (
       <Col lg={12}>
         <div className="newsletter-bx wow slideInUp">
           <Row>
             <Col lg={12} md={6} xl={5}>
               <h3>Suscríbete a mi boletín<br></br> & Nunca no te pierdas las actualizaciones.</h3>
+              {status === 'sending' && <Alert>Sending...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
             </Col>
             <Col md={6} xl={7}>
-              <form>
+              <form  onSubmit={handleSubmit}>
                 <div className="new-email-bx">
-                  <input type="email" placeholder="Su Email" />
-                  <button>Enviar</button>
+                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Su Email" />
+                  <button type="submit">Enviar</button>
                 </div>
               </form>
             </Col>
