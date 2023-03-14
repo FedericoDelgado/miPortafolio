f97d74140468546abefbd7df1c146a25a384e2a7
Federico Delgado <delgadofefe@gmail.com>
2023-03-13 21:48:49 -0300

    add certificados

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import './Modal.css';
 
-const Modal = ({ image, onClose }) => {
+function MyModal(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <img src={image} alt="Modal" />
-        <button className="modal-close" onClick={onClose}>X</button>
-      </div>
-    </div>
+    <>
+      <Button className='skill-button' variant="primary" onClick={handleShow}>
+      {props.buttonText}
+      </Button>
+
+      <Modal  show={show} onHide={handleClose}>
+        <div className="modal-dialog modal-dialog-centered">
+          <div className="modal-content">
+            <img src={props.image} alt="Imagen" className="skill-button" />
+          </div>
+        </div>
+      </Modal>
+    </>
   );
-};
+}
 
-Modal.propTypes = {
-  image: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired
-};
-
-export default Modal;
+export default MyModal;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import React from 'react';
+
+import MyModal from "./Modal";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
@@ -6,91 +9,105 @@ import Carousel from "react-multi-carousel";
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
-import Modal from "./Modal";
-
-import Button from 'react-bootstrap/Button';
+import colorSharp from "../assets/img/color-sharp.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CertificadoBackend from '../assets/img/CertificadoBackend.png';
+import CertificadoDesarrolloWeb from '../assets/img/CertificadoDesarrolloWeb.png';
+import CertificadoFronted from '../assets/img/CertificadoFronted.png';
+import CertificadoFullStack from '../assets/img/CertificadoFullStack.png';
+import CertificadoJS from '../assets/img/CertificadoJS.png';
+import CertificadoReact from '../assets/img/CertificadoReact.png';
+import CertificadoRedes from '../assets/img/Redes.png';
+import CertificadoReparacion from '../assets/img/Reparacion.png';
+import CertificadoReparacion2 from '../assets/img/Reparacion2.png';
+
+
 export const Skills = () => {
+  const [alertImage, setAlertImage] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    const [alertImage, setAlertImage] = useState("");
-    const [showModal, setShowModal] = useState(false); // Agregamos el estado para el modal
-  
-    const handleButtonClick = (image) => {
-      setAlertImage(image);
-      setShowModal(true); // Mostramos el modal cuando se hace clic en el botón
-    };
-  
-    const handleModalClose = () => {
-      setAlertImage("");
-      setShowModal(false); // Ocultamos el modal cuando se hace clic en el botón de cierre
-    };
+  const handleButtonClick = (imageUrl) => {
+    setAlertImage(imageUrl);
+    setShowModal(true);
+  };
 
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    
+  const handleModalClose = () => {
+    setAlertImage("");
+    setShowModal(false);
+  };
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Habilidades</h2>
-                        <p>Alguno de los principales lenguajes que manejo.<br></br> Sin implementar los framework de cada uno.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <div><img src={meter1} alt="Image" /></div>
-                                <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>
-                            <div className="item">
-                            <div><img src={meter2} alt="Image" /></div>
-                            <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>
-                            <div className="item">
-                            <div><img src={meter3} alt="Image" /></div>
-                            <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>
-                            <div className="item">
-                            <div><img src={meter1} alt="Image" /></div>
-                            <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>
-                            <div className="item">
-                            <div><img src={meter2} alt="Image" /></div>
-                            <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>
-                            <div className="item">
-                            <div><img src={meter3} alt="Image" /></div>
-                            <button onClick={() => handleButtonClick(meter1)} className="skill-button">Html y Css</button>
-                            </div>    
-                        </Carousel>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Habilidades</h2>
+              <p>Alguno de los principales lenguajes que manejo.<br></br> Sin implementar los framework de cada uno.</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <div><img src={meter1} alt="Image" /></div>
+                  <MyModal image={CertificadoBackend} buttonText="Dev Backend"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter2} alt="Image" /></div>
+                  <MyModal image={CertificadoDesarrolloWeb} buttonText="Dev Web"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter3} alt="Image" /></div>
+                  <MyModal image={CertificadoFronted} buttonText="Dev Fronted"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter1} alt="Image" /></div>
+                  <MyModal image={CertificadoFullStack} buttonText="Dev FullStack"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter2} alt="Image" /></div>
+                  <MyModal image={CertificadoJS} buttonText="JavaScript"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter3} alt="Image" /></div>
+                  <MyModal image={CertificadoReact} buttonText="React"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter3} alt="Image" /></div>
+                  <MyModal image={CertificadoRedes} buttonText="Redes Informáticas"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter3} alt="Image" /></div>
+                  <MyModal image={CertificadoReparacion} buttonText="Reparación"/>
+                </div>
+                <div className="item">
+                  <div><img src={meter3} alt="Image" /></div>
+                  <MyModal image={CertificadoReparacion2} buttonText="Reparación Avanzada"/>
+                </div>
+              </Carousel>
                     </div>
                 </div>
             </div>
         </div>
         <img className="background-image-left" src={colorSharp} alt="Image" />
-        {showModal && (
-  <Modal show={showModal} handleClose={handleModalClose}>
-    <img src={alertImage} alt="alert" />
-  </Modal>
-)}
+
     </section>
     
   )
